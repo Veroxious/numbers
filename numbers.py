4b3cf6ee454796a808c76ba4bb363bb9c729a997
Veroxious <116383324+Veroxious@users.noreply.github.com>
2022-10-21 15:18:31 -0400

    second commit
    now has a subtraction function

--- numbers.py
+++ numbers.py
@@ -3,6 +3,13 @@ def sum():
     num2 = int(input("Second number:"))
     return num1+num2
 
+def subtract():
+    num1 = int(input("First number:"))
+    num2 = int(input("Second number:"))
+    return num1-num2
+
 def main():
     total = sum()
     print("The sum is:", total)
+    sub = subtract()
+    print("The subtraction is:", sub)
